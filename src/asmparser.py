d7ead108462c2fa28cea71d2c9fef90aeaef9ed8
vk <varunwell10@gmail.com>
2017-10-21 12:21:33 +0200

    Pyparsing import working. Basic print working and first 2 lines of instruction set is getting tokanized.

--- src/asmparser.py
+++ src/asmparser.py
@@ -1,5 +1,13 @@
-import sys
+#Parser file which defines grammer and parsing
 
 
+from pyparsing import Word, alphas
+greet = Word( alphas ) + Word ( alphas )  # <-- grammar defined here
+asmfileptr = open("../data/asm.txt",'r')
 
+asmfile = asmfileptr.read()
+
+#print(asmfile)
+
+print (asmfile, "->", greet.parseString( asmfile ))
 
